ab2560c00261880fa44568233fae65f215d6033a
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-06-13 10:34:33 +0530

    Create credential.py

---
+++ credential.py
@@ -0,0 +1,18 @@
+# Salesforce credentials
+
+# Find out Security Token:
+# https://<yoursalesforcehostname>/_ui/system/security/ResetApiTokenEdit?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DPersonalInfo&setupid=ResetApiToken
+# https://avi.my.salesforce.com/_ui/system/security/ResetApiTokenEdit?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DPersonalInfo&setupid=ResetApiToken
+
+username='someone@company.com'
+password='blablabla'
+security_token='NSUgqlsdsfgerhvdsvVbwjs'
+
+# Gmail Credentials
+ORG_EMAIL= "@gmail.com"
+FROM_EMAIL= "someone@gmail.com"
+FROM_PWD= 'blablablabla'
+
+# Slack Credentials
+token='xoxp-343554354-30915543575-3595333110-95d5978ecda23553g3r342822'
+channel='support-group'
